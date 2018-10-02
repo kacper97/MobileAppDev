317534a6b1dd97a10f7ae529507633cfa2d43088
unknown <kacperwoloszyn4@gmail.com>
2018-10-02 10:32:36 +0100

    adding cancel button and id's

---
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -0,0 +1,62 @@
+package org.wit.placemark.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_placemark.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.wit.placemark.R
+import org.wit.placemark.main.MainApp
+import org.wit.placemark.models.PlacemarkModel
+
+class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
+
+  var placemark = PlacemarkModel()
+  lateinit var app: MainApp
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_placemark)
+    toolbarAdd.title = title
+    setSupportActionBar(toolbarAdd)
+    info("Placemark Activity started..")
+
+    app = application as MainApp
+
+    if (intent.hasExtra("placemark_edit")) {
+      placemark = intent.extras.getParcelable<PlacemarkModel>("placemark_edit")
+      placemarkTitle.setText(placemark.title)
+      description.setText(placemark.description)
+    }
+
+    btnAdd.setOnClickListener() {
+      placemark.title = placemarkTitle.text.toString()
+      placemark.description = description.text.toString()
+      if (placemark.title.isNotEmpty()) {
+        app.placemarks.create(placemark.copy())
+        info("add Button Pressed: $placemarkTitle")
+        setResult(AppCompatActivity.RESULT_OK)
+        finish()
+      } else {
+        toast("Please Enter a title")
+      }
+    }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_placemark, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item?.itemId) {
+      R.id.item_cancel -> {
+        finish()
+      }
+    }
+    return super.onOptionsItemSelected(item)
+  }
+}

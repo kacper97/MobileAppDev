0174b0e78db114ff07e4979384f3c190650ea3ff
unknown <kacperwoloszyn4@gmail.com>
2018-10-19 10:18:41 +0100

    Adding Json updating Main app and adding gson in gradle

--- app/src/main/java/org/wit/placemark/main/MainApp.kt
+++ app/src/main/java/org/wit/placemark/main/MainApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.wit.placemark.models.PlacemarkMemStore
+import org.wit.
 import org.wit.placemark.models.PlacemarkStore
 
 class MainApp : Application(), AnkoLogger {

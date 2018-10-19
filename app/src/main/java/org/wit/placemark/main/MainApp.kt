9ad7e16b4f8c117fa2730d4756d009895a998c82
unknown <kacperwoloszyn4@gmail.com>
2018-10-19 10:19:56 +0100

    Main app switch from mem store to JSON

--- app/src/main/java/org/wit/placemark/main/MainApp.kt
+++ app/src/main/java/org/wit/placemark/main/MainApp.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.wit.placemark.models.PlacemarkMemStore
-import org.wit.
+import org.wit.placemark.models.PlacemarkJSONStore
 import org.wit.placemark.models.PlacemarkStore
 
 class MainApp : Application(), AnkoLogger {
@@ -13,7 +13,7 @@ class MainApp : Application(), AnkoLogger {
 
   override fun onCreate() {
     super.onCreate()
-    placemarks = PlacemarkMemStore()
+    placemarks = PlacemarkJSONStore()
     info("Placemark started")
   }
 }

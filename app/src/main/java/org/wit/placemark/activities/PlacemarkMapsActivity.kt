02708a63ccba45211fb87f0b23e880951105945a
unknown <kacperwoloszyn4@gmail.com>
2018-11-09 09:51:49 +0000

    Changing files to suit layout of lab

--- app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
@@ -14,10 +14,8 @@ class PlacemarkMapsActivity : AppCompatActivity() {
     setContentView(R.layout.activity_placemark_maps)
     setSupportActionBar(toolbar)
 
-    fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null).show()
+
     }
-  }
+
 
 }

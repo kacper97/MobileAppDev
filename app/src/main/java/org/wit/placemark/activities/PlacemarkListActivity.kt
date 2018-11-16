ed07afd26a71fcb8304fac202d058ac0087a8e11
unknown <kacperwoloszyn4@gmail.com>
2018-11-16 10:06:46 +0000

    Placemark list split into presenter and list

--- app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
@@ -6,28 +6,24 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.*
 import kotlinx.android.synthetic.main.activity_placemark_list.*
-import org.jetbrains.anko.intentFor
-import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.startActivityForResult
 import org.wit.placemark.R
-import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
-
 
 class PlacemarkListActivity : AppCompatActivity(), PlacemarkListener {
 
-  lateinit var app: MainApp
+  lateinit var presenter: PlacemarkListPresenter
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark_list)
-    app = application as MainApp
     toolbarMain.title = title
     setSupportActionBar(toolbarMain)
 
+    presenter = PlacemarkListPresenter(this)
     val layoutManager = LinearLayoutManager(this)
     recyclerView.layoutManager = layoutManager
-    loadPlacemarks()
+    recyclerView.adapter = PlacemarkAdapter(presenter.getPlacemarks(), this)
+    recyclerView.adapter?.notifyDataSetChanged()
   }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -37,27 +33,18 @@ class PlacemarkListActivity : AppCompatActivity(), PlacemarkListener {
 
   override fun onOptionsItemSelected(item: MenuItem?): Boolean {
     when (item?.itemId) {
-      R.id.item_add -> startActivityForResult<PlacemarkActivity>(200)
-      R.id.item_map -> startActivity<PlacemarkMapsActivity>()
+      R.id.item_add -> presenter.doAddPlacemark()
+      R.id.item_map -> presenter.doShowPlacemarksMap()
     }
     return super.onOptionsItemSelected(item)
   }
 
   override fun onPlacemarkClick(placemark: PlacemarkModel) {
-    startActivityForResult(intentFor<PlacemarkActivity>().putExtra("placemark_edit", placemark), AppCompatActivity.RESULT_OK)
+    presenter.doEditPlacemark(placemark)
   }
 
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    loadPlacemarks()
-    super.onActivityResult(requestCode, resultCode, data)
-  }
-
-  private fun loadPlacemarks(){
-    showPlacemarks(app.placemarks.findAll())
-  }
-
-  fun showPlacemarks(placemarks: List<PlacemarkModel>){
-    recyclerView.adapter = PlacemarkAdapter(placemarks,this)
     recyclerView.adapter?.notifyDataSetChanged()
+    super.onActivityResult(requestCode, resultCode, data)
   }
 }

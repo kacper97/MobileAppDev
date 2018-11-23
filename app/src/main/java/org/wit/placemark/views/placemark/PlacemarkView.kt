8b248ae4ce4bbb123649f3b2873cc63992a6d81c
unknown <kacperwoloszyn4@gmail.com>
2018-11-23 10:24:48 +0000

    placemark view

--- app/src/main/java/org/wit/placemark/views/placemark/PlacemarkView.kt
+++ app/src/main/java/org/wit/placemark/views/placemark/PlacemarkView.kt
@@ -70,5 +70,31 @@ class PlacemarkView : BaseView(), AnkoLogger {
   override fun onBackPressed() {
     presenter.doCancel()
   }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    mapView.onDestroy()
+  }
+
+  override fun onLowMemory() {
+    super.onLowMemory()
+    mapView.onLowMemory()
+  }
+
+  override fun onPause() {
+    super.onPause()
+    mapView.onPause()
+  }
+
+  override fun onResume() {
+    super.onResume()
+    mapView.onResume()
+  }
+
+  override fun onSaveInstanceState(outState: Bundle?) {
+    super.onSaveInstanceState(outState)
+    mapView.onSaveInstanceState(outState)
+  }
+}
 }
 
